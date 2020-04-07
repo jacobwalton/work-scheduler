96bb6b3e0ca2128d7a5ebd2b7dec2f47baf908e5
Jacob Walton <jawalton6616@gmail.com>
2020-04-07 17:23:58 -0500

    added current date

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function getDate() {
+
   var today = moment();
   var getDate = $("#currentDay");
   getDate.addClass("description");
   $(".description").text(today.format("MM-DD-YYYY"));
   $("#currentDay").append(getDate);
-}
+
 // append text to jumbotron to display current day (day of week, month, date)
 
 //create conditional to see if time is past present or future(Use moment.js momentjs.com)
