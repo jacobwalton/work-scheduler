a567eb87f05e845e452f33e9e136547277cbf5dd
Jacob Walton <jawalton6616@gmail.com>
2020-04-07 17:18:31 -0500

    added conditional to add class to rows("present" class not working

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-    var today = moment();
-    var getDate=$("#currentDay");
-    getDate.addClass("description");
-    $(".description").text(today.format('MM-DD-YYYY'));
-    $("#currentDay").append(getDate);
-    
-
-
+function getDate() {
+  var today = moment();
+  var getDate = $("#currentDay");
+  getDate.addClass("description");
+  $(".description").text(today.format("MM-DD-YYYY"));
+  $("#currentDay").append(getDate);
+}
 // append text to jumbotron to display current day (day of week, month, date)
-
 
 //create conditional to see if time is past present or future(Use moment.js momentjs.com)
 //create using empty string
 //assign to appropriate class
 
-//save input to local storage when save btn is clicked
+var hour = parseInt($("#hour").text());
+var timeBlock = moment();
+var now = moment(timeBlock.format("hh"));
+console.log(now);
+if (hour > now) {
+  $("textarea").addClass("future");
+} else if (hour == now) {
+  $("textarea").addClass("present");
+} else {
+  $("textarea").addClass("past");
+}
 
+//save input to local storage when save btn is clicked
