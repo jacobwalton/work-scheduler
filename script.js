49ffb947134cf1d94bfac81c65f347dfdf9f4f5f
Jacob Walton <jawalton6616@gmail.com>
2020-04-09 10:55:24 -0500

    working color changes for textareas

--- script.js
+++ script.js
@@ -1,26 +1,82 @@
+var today = moment();
+var getDate = $("#currentDay");
+getDate.addClass("description");
+$(".description").text(today.format("MM-DD-YYYY"));
+$("#currentDay").append(getDate);
 
-  var today = moment();
-  var getDate = $("#currentDay");
-  getDate.addClass("description");
-  $(".description").text(today.format("MM-DD-YYYY"));
-  $("#currentDay").append(getDate);
-
-// append text to jumbotron to display current day (day of week, month, date)
-
-//create conditional to see if time is past present or future(Use moment.js momentjs.com)
-//create using empty string
-//assign to appropriate class
-
-var hour = parseInt($("#hour").text());
 var timeBlock = moment();
-var now = moment(timeBlock.format("hh"));
-console.log(now);
-if (hour > now) {
-  $("textarea").addClass("future");
-} else if (hour == now) {
-  $("textarea").addClass("present");
+var now = moment(timeBlock.format("HH"));
+
+if ($("#9am").attr('value') > now._i) {
+  $("#9amrow").addClass("future");
+} else if ($("#9am").attr('value') < now._i) {
+  $("#9amrow").addClass("past");
 } else {
-  $("textarea").addClass("past");
+  $("#9amrow").addClass("present");
+}
+if ($("#10am").attr('value') > now._i) {
+  $("#10amrow").addClass("future");
+} else if ($("#10am").attr('value') < now._i) {
+  $("#10amrow").addClass("past");
+} else {
+  $("#10amrow").addClass("present");
+}
+if ($("#11am").attr('value') > now._i) {
+  $("#11amrow").addClass("future");
+} else if ($("#11am").attr('value') < now._i) {
+  $("#11amrow").addClass("past");
+} else {
+  $("#11amrow").addClass("present");
+}
+if ($("#12pm").attr('value') > now._i) {
+  $("#12pmrow").addClass("future");
+} else if ($("#12pm").attr('value') < now._i) {
+  $("#12pmrow").addClass("past");
+} else {
+  $("#12pmrow").addClass("present");
+}
+if ($("#1pm").attr('value') > now._i) {
+  $("#1pmrow").addClass("future");
+} else if ($("#1pm").attr('value') < now._i) {
+  $("#1pmrow").addClass("past");
+} else {
+  $("#1pmrow").addClass("present");
+}
+if ($("#2pm").attr('value') > now._i) {
+  $("#2pmrow").addClass("future");
+} else if ($("#2pm").attr('value') < now._i) {
+  $("#2pmrow").addClass("past");
+} else {
+  $("#2pmrow").addClass("present");
+}
+if ($("#3pm").attr('value') > now._i) {
+  $("#3pmrow").addClass("future");
+} else if ($("#3pm").attr('value') < now._i) {
+  $("#3pmrow").addClass("past");
+} else {
+  $("#3pmrow").addClass("present");
+}
+if ($("#4pm").attr('value') > now._i) {
+  $("#4pmrow").addClass("future");
+} else if ($("#4pm").attr('value') < now._i) {
+  $("#4pmrow").addClass("past");
+} else {
+  $("#4pmrow").addClass("present");
+}
+if ($("#5pm").attr('value') > now._i) {
+  $("#5pmrow").addClass("future");
+} else if ($("#5pm").attr('value') < now._i) {
+  $("#5pmrow").addClass("past");
+} else {
+  $("#5pmrow").addClass("present");
 }
 
-//save input to local storage when save btn is clicked
+// //save input to local storage when save btn is clicked
+// function storeInput (){
+// localStorage.setItem('eventInput', input);
+// };
+
+// $('button').on('click', function(){
+// rowText = input;
+
+// localStorage.getItem('eventInput', input);
